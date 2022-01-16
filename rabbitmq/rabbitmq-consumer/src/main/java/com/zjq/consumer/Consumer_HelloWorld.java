1cbefadd6fa5c58c7f7f386310724f10b9d22f59
Jack <1187625098@qq.com>
2022-01-16 19:35:35 +0800

    rabbitmq 订阅模式

--- rabbitmq/rabbitmq-consumer/src/main/java/com/zjq/consumer/Consumer_HelloWorld.java
+++ rabbitmq/rabbitmq-consumer/src/main/java/com/zjq/consumer/Consumer_HelloWorld.java
@@ -12,21 +12,8 @@ import java.util.concurrent.TimeoutException;
 public class Consumer_HelloWorld {
     public static void main(String[] args) throws IOException, TimeoutException {
 
-        //1.创建连接工厂
-        ConnectionFactory factory = new ConnectionFactory();
-        //2. 设置参数
-        //ip  默认值 localhost
-        factory.setHost("localhost");
-        //端口  默认值 5672
-        factory.setPort(5672);
-        //虚拟机 默认值/
-        factory.setVirtualHost("/zjq");
-        //用户名 默认 guest
-        factory.setUsername("guest");
-        //密码 默认值 guest
-        factory.setPassword("guest");
         //3. 创建连接 Connection
-        Connection connection = factory.newConnection();
+        Connection connection = ConnectionUtil.getConnection();
         //4. 创建Channel
         Channel channel = connection.createChannel();
         //5. 创建队列Queue
--- rabbitmq/rabbitmq-consumer/src/main/java/com/zjq/consumer/Consumer_PubSub1.java
+++ rabbitmq/rabbitmq-consumer/src/main/java/com/zjq/consumer/Consumer_PubSub1.java
@@ -11,28 +11,13 @@ import java.util.concurrent.TimeoutException;
 public class Consumer_PubSub1 {
     public static void main(String[] args) throws IOException, TimeoutException {
 
-        //1.创建连接工厂
-        ConnectionFactory factory = new ConnectionFactory();
-        //2. 设置参数
-        //ip  默认值 localhost
-        factory.setHost("localhost");
-        //端口  默认值 5672
-        factory.setPort(5672);
-        //虚拟机 默认值/
-        factory.setVirtualHost("/zjq");
-        //用户名 默认 guest
-        factory.setUsername("guest");
-        //密码 默认值 guest
-        factory.setPassword("guest");
         //3. 创建连接 Connection
-        Connection connection = factory.newConnection();
+        Connection connection = ConnectionUtil.getConnection();
         //4. 创建Channel
         Channel channel = connection.createChannel();
 
-
         String queue1Name = "test_fanout_queue1";
         String queue2Name = "test_fanout_queue2";
-
 
         /*
         basicConsume(String queue, boolean autoAck, Consumer callback)
