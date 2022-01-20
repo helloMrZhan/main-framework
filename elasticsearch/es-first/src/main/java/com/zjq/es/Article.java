78f6eed94138b2c9d01ea47b8b0139d8b62e60ed
zhanjq <1187625098@qq.com>
2022-01-20 13:09:45 +0800

    elasticsearch入门案例

---
+++ elasticsearch/es-first/src/main/java/com/zjq/es/Article.java
@@ -0,0 +1,35 @@
+package com.zjq.es;
+
+/***
+ *
+ * @author zjq
+ */
+public class Article {
+    private long id;
+    private String title;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
