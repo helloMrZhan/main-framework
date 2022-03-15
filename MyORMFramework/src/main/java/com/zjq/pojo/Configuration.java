3c1b67870223924b83d6988d6d81306be994f963
zhanjq <1187625098@qq.com>
2022-03-15 14:33:27 +0800

    数据源和映射语句配置实体

---
+++ MyORMFramework/src/main/java/com/zjq/pojo/Configuration.java
@@ -0,0 +1,24 @@
+package com.zjq.pojo;
+
+import lombok.Data;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 映射语句实体
+ * @author zjq
+ * @date 2022/3/15
+ */
+@Data
+public class Configuration {
+
+    private DataSource dataSource;
+
+    /**
+     *  key: statementId  value:封装好的mappedStatement对象
+     */
+    Map<String,MappedStatement> mappedStatementMap = new HashMap<>();
+
+}
---
+++ MyORMFramework/src/main/java/com/zjq/pojo/MappedStatement.java
@@ -0,0 +1,26 @@
+package com.zjq.pojo;
+
+import lombok.Data;
+
+/**
+ * 数据源和映射语句集合实体
+ * @author zjq
+ * @date 2022/3/15
+ */
+@Data
+public class MappedStatement {
+
+    private String id;
+    /**
+     * sql语句
+     */
+    private String sql;
+    /**
+     * 参数值类型
+     */
+    private String parameterType;
+    /**
+     * 返回值类型
+     */
+    private String resultType;
+}
