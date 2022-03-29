23598dfcdff2e2550e586d8fa61989a59a481aee
zhanjq <1187625098@qq.com>
2022-03-29 13:43:47 +0800

    执⾏commit()操作，⼆级缓存数据清空

--- mybatis/mybatis-cache/src/test/java/com/zjq/test/MapperTest.java
+++ mybatis/mybatis-cache/src/test/java/com/zjq/test/MapperTest.java
@@ -97,4 +97,33 @@ public class MapperTest {
         System.out.println(u2);
         sqlSession2.close();
     }
+
+    /**
+     * 执⾏commit()操作，⼆级缓存数据清空
+     * @throws IOException
+     */
+    @Test
+    public void testTwoCache2() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //根据 sqlSessionFactory 产⽣ session
+        SqlSession sqlSession1 = sessionFactory.openSession();
+        SqlSession sqlSession2 = sessionFactory.openSession();
+        SqlSession sqlSession3 = sessionFactory.openSession();
+        UserMapper userMapper1 = sqlSession1.getMapper(UserMapper. class );
+        UserMapper userMapper2 = sqlSession2.getMapper(UserMapper. class );
+        UserMapper userMapper3 = sqlSession3.getMapper(UserMapper. class );
+        //第⼀次查询，发出sql语句，并将查询的结果放⼊缓存中
+        User u1 = userMapper1.selectUserByUserId( 1 );
+        System.out.println(u1);
+        sqlSession1.close(); //第⼀次查询完后关闭sqlSession
+        //执⾏更新操作，commit()
+        u1.setUsername( "zjq" );
+        userMapper3.updateById(u1);
+        sqlSession3.commit();
+        //第⼆次查询，由于上次更新操作，缓存数据已经清空(防⽌数据脏读)，这⾥必须再次发出sql语句
+        User u2 = userMapper2.selectUserByUserId( 1 );
+        System.out.println(u2);
+        sqlSession2.close();
+    }
 }
