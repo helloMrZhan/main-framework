40212c2f18eac0c493a8f2712d580c622fc29fa8
Jack <1187625098@qq.com>
2022-04-05 21:47:36 +0800

    文档补充

--- spring/spring-ioc/src/main/java/com/zjq/factory/DynamicFactory.java
+++ spring/spring-ioc/src/main/java/com/zjq/factory/DynamicFactory.java
@@ -3,6 +3,10 @@ package com.zjq.factory;
 import com.zjq.dao.UserDao;
 import com.zjq.dao.impl.UserDaoImpl;
 
+/**
+ * 普通工厂
+ * @author zjq
+ */
 public class DynamicFactory {
 
     public UserDao getUserDao(){
--- spring/spring-ioc/src/main/java/com/zjq/factory/StaticFactory.java
+++ spring/spring-ioc/src/main/java/com/zjq/factory/StaticFactory.java
@@ -3,10 +3,15 @@ package com.zjq.factory;
 import com.zjq.dao.UserDao;
 import com.zjq.dao.impl.UserDaoImpl;
 
+/**
+ * 静态工厂
+ * @author zjq
+ */
 public class StaticFactory {
 
     public static UserDao getUserDao(){
         return new UserDaoImpl();
     }
+
 
 }
