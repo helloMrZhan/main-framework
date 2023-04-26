2414ae50517fb2bb8aa7c0bdb2337d6eda9b4087
zjq <1187625098@qq.com>
2023-04-26 14:41:10 +0800

    feature：mybatis 动态sql语句

--- mybatis/mybatis-dao-learn/mybatis-mapper/src/test/java/com/zjq/test/MapperTest.java
+++ mybatis/mybatis-dao-learn/mybatis-mapper/src/test/java/com/zjq/test/MapperTest.java
@@ -16,28 +16,35 @@ import java.util.List;
 public class MapperTest {
 
     @Test
-    public void test1() throws IOException {
+    public void findByCondition() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //获得MyBatis框架生成的UserMapper接口的实现类
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        //模拟条件user
+        User condition = new User();
+        condition.setId(2);
+        condition.setUsername("共饮一杯无");
+
+        List<User> userList = mapper.findByCondition(condition);
+        System.out.println(userList);
+    }
+
+
+    @Test
+    public void findByIds() throws IOException {
         InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-
-        //模拟条件user
-        User condition = new User();
-        //condition.setId(1);
-        condition.setUsername("zjq");
-        //condition.setPassword("123");
-
-        //List<User> userList = mapper.findByCondition(condition);
-
         //模拟ids的数据
-        List<Integer> ids = new ArrayList<Integer>();
+        List<Integer> ids = new ArrayList<>();
         ids.add(1);
         ids.add(2);
 
         List<User> userList = mapper.findByIds(ids);
         System.out.println(userList);
     }
-
 }
